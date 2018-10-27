7a34076b2da5a23cf4453fe44e5fcad6fca8313e
Niall Riordan <nriordan.dev@gmail.com>
2018-10-27 14:22:51 +0200

    Add POST note route

---
+++ create.js
@@ -0,0 +1,48 @@
+import uuid from "uuid";
+import AWS from "aws-sdk";
+
+AWS.config.update({ region: "us-east-1" });
+const dynamoDb = new AWS.DynamoDB.DocumentClient();
+
+export function main(event, context, callback) {
+  // Request body is passed in as a JSON encoded string in 'event.body'
+  const data = JSON.parse(event.body);
+
+  const params = {
+    TableName: "notes",
+    Item: {
+      userId: event.requestContext.identity.cognitoIdentityId,
+      noteId: uuid.v1(),
+      content: data.content,
+      attachment: data.attachment,
+      createdAt: Date.now()
+    }
+  };
+
+  dynamoDb.put(params, (error, data) => {
+    // Set response headers to enable CORS (Cross-Origin Resource Sharing)
+    const headers = {
+      "Access-Control-Allow-Origin": "*",
+      "Access-Control-Allow-Credentials": true
+    };
+
+    // Return status code 500 on error
+    if (error) {
+      const response = {
+        statusCode: 500,
+        headers,
+        body: JSON.stringify({ status: false })
+      };
+      callback(null, response);
+      return;
+    }
+
+    // Return status code 200 and the newly created item
+    const response = {
+      statusCode: 200,
+      headers,
+      body: JSON.stringify(params.Item)
+    };
+    callback(null, response);
+  });
+}
