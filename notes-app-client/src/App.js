0cbeb15a7789b4942c064da7b1793c38546a1634
Niall Riordan <nriordan.dev@gmail.com>
2018-10-28 12:53:08 +0100

    Initial frontend commit using react-create-app

---
+++ notes-app-client/src/App.js
@@ -0,0 +1,25 @@
+import React, { Component } from "react";
+import { Link } from "react-router-dom";
+import { Navbar } from "react-bootstrap";
+import Routes from "./Routes";
+import "./App.css";
+
+class App extends Component {
+  render() {
+    return (
+      <div className="App container">
+        <Navbar fluid collapseOnSelect>
+          <Navbar.Header>
+            <Navbar.Brand>
+              <Link to="/">Scratch</Link>
+            </Navbar.Brand>
+            <Navbar.Toggle />
+          </Navbar.Header>
+        </Navbar>
+        <Routes />
+      </div>
+    );
+  }
+}
+
+export default App;
---
+++ notes-app-client/src/containers/Home.js
@@ -0,0 +1,16 @@
+import React, { Component } from "react";
+import "./Home.css";
+
+export default class Home extends Component {
+  render() {
+    return (
+      <div className="Home">
+        <div className="lander">
+          <h1>Niall's notes app</h1>
+          <p>A simple note taking app</p>
+        </div>
+      </div>
+    );
+  }
+}
+
