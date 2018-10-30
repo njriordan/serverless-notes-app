73b758e05def2f936849004be6c0e9f120adb8ad
Niall Riordan <nriordan.dev@gmail.com>
2018-10-30 08:09:21 +0100

    Improve login behaviour

--- notes-app-client/src/App.js
+++ notes-app-client/src/App.js
@@ -1,7 +1,8 @@
 import React, { Component, Fragment } from "react";
-import { Link } from "react-router-dom";
+import { Link, withRouter } from "react-router-dom";
 import { Nav, Navbar, NavItem } from "react-bootstrap";
 import { LinkContainer } from "react-router-bootstrap";
+import { Auth } from "aws-amplify";
 import Routes from "./Routes";
 import "./App.css";
 
@@ -11,26 +12,43 @@ class App extends Component {
     super(props);
 
     this.state = {
-      isAuthenticated: false
+      isAuthenticated: false,
+      isAuthenticating: true
     };
   }
+
+  async componentDidMount() {
+    try {
+      await Auth.currentSession();
+      this.userHasAuthenticated(true);
+    }
+    catch(e) {
+      if (e !== 'No current user') {
+        alert(e);
+      }
+    }
+  
+    this.setState({ isAuthenticating: false });
+  }  
 
   userHasAuthenticated = authenticated => {
     this.setState({ isAuthenticated: authenticated });
   }
 
-  handleLogout = event => {
+  handleLogout = async event => {
+    await Auth.signOut();
     this.userHasAuthenticated(false);
+    this.props.history.push("/login");
   }
 
   render() {
-
     const childProps = {
       isAuthenticated: this.state.isAuthenticated,
       userHasAuthenticated: this.userHasAuthenticated
     };
-    
+  
     return (
+      !this.state.isAuthenticating &&
       <div className="App container">
         <Navbar fluid collapseOnSelect>
           <Navbar.Header>
@@ -59,7 +77,6 @@ class App extends Component {
       </div>
     );
   }
-  
 }
 
-export default App;
+export default withRouter(App);
