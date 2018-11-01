385817f09084876164eb781bc8c4d470a67196b5
Niall Riordan <nriordan.dev@gmail.com>
2018-11-01 11:26:56 +0100

    Delete note

--- notes-app-client/src/containers/Notes.js
+++ notes-app-client/src/containers/Notes.js
@@ -99,6 +99,10 @@ export default class Notes extends Component {
     }
   }  
   
+  deleteNote() {
+    return API.del("notes", `/notes/${this.props.match.params.id}`);
+  }
+
   handleDelete = async event => {
     event.preventDefault();
   
@@ -111,8 +115,16 @@ export default class Notes extends Component {
     }
   
     this.setState({ isDeleting: true });
-  }
   
+    try {
+      await this.deleteNote();
+      this.props.history.push("/");
+    } catch (e) {
+      alert(e);
+      this.setState({ isDeleting: false });
+    }
+  }  
+
   render() {
     console.log(this.state)
     return (
