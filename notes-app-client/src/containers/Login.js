c5e10bb54d6b9f1770a6313aef197e73bb4375d7
Niall Riordan <nriordan.dev@gmail.com>
2018-11-01 12:25:22 +0100

    Redirect when attempting to access a page without logging in

--- notes-app-client/src/containers/Login.js
+++ notes-app-client/src/containers/Login.js
@@ -35,7 +35,6 @@ export default class Login extends Component {
     try {
       await Auth.signIn(this.state.email, this.state.password);
       this.props.userHasAuthenticated(true);
-      this.props.history.push("/");
     } catch (e) {
       alert(e.message);
       this.setState({ isLoading: false });
