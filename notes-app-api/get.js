1289c299a9307b445825ffbfbc8f7df337690513
Niall Riordan <nriordan.dev@gmail.com>
2018-11-07 07:33:13 +0100

    Use environment variables in lambda functions

--- notes-app-api/get.js
+++ notes-app-api/get.js
@@ -3,7 +3,7 @@ import { success, failure } from "./libs/response-lib";
 
 export async function main(event, context, callback) {
   const params = {
-    TableName: "notes",
+    TableName: process.env.tableName,
     Key: {
       userId: event.requestContext.identity.cognitoIdentityId,
       noteId: event.pathParameters.id
--- notes-app-api/list.js
+++ notes-app-api/list.js
@@ -3,7 +3,7 @@ import { success, failure } from "./libs/response-lib";
 
 export async function main(event, context, callback) {
   const params = {
-    TableName: "notes",
+    TableName: process.env.tableName,
     KeyConditionExpression: "userId = :userId",
     ExpressionAttributeValues: {
       ":userId": event.requestContext.identity.cognitoIdentityId
