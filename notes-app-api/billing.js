af965a61e98e4577e6c85ec05e1c0d7cd82e1d7a
Niall Riordan <nriordan.dev@gmail.com>
2018-11-08 07:26:21 +0100

    Add a billing endpoint

---
+++ notes-app-api/billing.js
@@ -0,0 +1,25 @@
+import stripePackage from "stripe";
+import { calculateCost } from "./libs/billing-lib";
+import { success, failure } from "./libs/response-lib";
+
+export async function main(event, context) {
+  const { storage, source } = JSON.parse(event.body);
+  const amount = calculateCost(storage);
+  const description = "Notes app usage";
+
+  // Load secret key from the env variables
+  const stripe = stripePackage(process.env.stripeSecretKey);
+
+  try {
+    await stripe.charges.create({
+      source,
+      amount,
+      description,
+      currency: "eur"
+    });
+    return success({ status: true });
+  } catch (e) {
+    return failure({ message: e.message });
+  }
+}
+
