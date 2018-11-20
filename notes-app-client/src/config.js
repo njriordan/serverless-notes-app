5f1ae5b7ed92d1b77d4f122641bb7b44847530f1
Niall Riordan <nriordan.dev@gmail.com>
2018-11-20 07:27:15 +0100

    Add stripe key to config

--- notes-app-client/src/config.js
+++ notes-app-client/src/config.js
@@ -13,5 +13,6 @@ export default {
     USER_POOL_ID: process.env.REACT_APP_USER_POOL_ID,
     APP_CLIENT_ID: process.env.REACT_APP_APP_CLIENT_ID,
     IDENTITY_POOL_ID: process.env.REACT_APP_IDENTITY_POOL_ID
-  }
+  },
+  STRIPE_KEY: process.env.REACT_APP_STRIPE_KEY,
 };
